1857cf30096fe96d3fd5308c11444ae99106fe24
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-10-22 10:32:37 +0300

    More tests

--- GolangSnippets/src/pathbuilder/path_test.go
+++ GolangSnippets/src/pathbuilder/path_test.go
@@ -14,4 +14,24 @@ func TestPath(t *testing.T) {
 
 	path = path.Cut(3)
 	require.Equal(t, Path([]int32{1, 2}), path)
+
+	failures := []interface{}{
+		"123",
+		int64(0x7fffffff) + int64(1),
+		-int64(0x80000000) - int64(3),
+		uint32(0x80000000),
+		uint64(0x80000000),
+		int(0x7fffffff) + 1,
+		uint(0x80000000),
+	}
+	for _, failure := range failures {
+		func() {
+			defer func() {
+				if r := recover(); r == nil {
+					t.Fatalf("Conversion of %v (type %T) must cause a panic", failure, failure)
+				}
+			}()
+			path = path.Append(failure)
+		}()
+	}
 }
